e1c47eff3fbcc495ed4ea1dbb221cd5f063fe796
Nikita Shakhbazyan <129982391+nikitaShakhbazyan@users.noreply.github.com>
2025-01-02 23:10:35 +0400

    f

--- src/store/store.ts
+++ src/store/store.ts
@@ -23,7 +23,7 @@ const initialState = {
     },
   });
   
-  export type RootState = ReturnType<typeof store.getState>; // Тип для состояния
-  export type AppDispatch = typeof store.dispatch; // Тип для dispatch
+  export type RootState = ReturnType<typeof store.getState>; 
+  export type AppDispatch = typeof store.dispatch;
   
   export default store;
